f218cb3f0c5f45f727e57bfd5866f9f5c4d10444
monpro <monproshen@gmail.com>
2019-08-10 21:30:33 +1000

    add answer module

---
+++ app/example/models/answers.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const {Schema, model} = mongoose;
+
+const answerSchema = new Schema({
+    __v: {type: Number, select: false},
+    content: {type: String, required: true},
+    answerer: {type: Schema.Types.ObjectId, ref: "User", required: true, select: false},
+    questionId: {type: String, required: true},
+    voteCount: {type: Number, required: true, default: 0}
+    },
+    {timestamp: true});
+
+module.exports = model("Answer", answerSchema);
